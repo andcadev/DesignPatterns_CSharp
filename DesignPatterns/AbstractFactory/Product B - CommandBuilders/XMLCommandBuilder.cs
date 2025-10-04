fd76a594dbad031af03926e06b7e6acaa891aebd
andcadev <andreacadei85@gmail.com>
2025-10-04 10:30:43 +0200

    Changed the XML commands (now they are more real)

--- DesignPatterns/AbstractFactory/Product B - CommandBuilders/XMLCommandBuilder.cs
+++ DesignPatterns/AbstractFactory/Product B - CommandBuilders/XMLCommandBuilder.cs
@@ -5,10 +5,15 @@ namespace AbstractFactory
     public class XmlCommandBuilder : ICommandBuilder
     {
         // Build command to set the temperature set point
-        public string BuildSetTemperature(int temperature) => new XElement("SetTemperature",
-            new XElement("Value", temperature)).ToString(SaveOptions.DisableFormatting);
+        public string BuildSetTemperature(int temperature) => new XElement("Command",
+            new XElement("SetTemperature",
+            new XElement("Value", temperature)
+        )
+    ).ToString(SaveOptions.DisableFormatting);
+
 
         // Build command to start the Oven
-        public string BuildStartCycle() => new XElement("StartCycle").ToString(SaveOptions.DisableFormatting);
+        public string BuildStartCycle() => new XElement("Command",
+            new XElement("StartCycle")).ToString(SaveOptions.DisableFormatting);
     }
 }
